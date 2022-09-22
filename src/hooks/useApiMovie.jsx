fe5b286dd6317473fd3bf4519ff73844181cfaee
Jean <jeanncavalcante@gmail.com>
2022-09-22 12:50:09 -0300

    estruturando o axios

---
+++ src/hooks/useApiMovie.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const useApiMovie = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const GetMovie = async () => {
+      setLoading(true);
+
+      const api = await axios.get(url, {
+        params: {
+          api_key: "0a63895da26658ca9dd1ffeb20159d01",
+          language: "pt-BR",
+        },
+      });
+
+      setData(api.data.results);
+
+      setLoading(true);
+      console.log(api.data.results);
+    };
+
+    GetMovie();
+  }, []);
+
+  return { data, loading };
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,10 @@
+import { useApiMovie } from "../../hooks/useApiMovie";
+
 const Home = () => {
+  const url = "https://api.themoviedb.org/3/movie/now_playing";
+  const { data, loading } = useApiMovie(url);
+
+  // console.log(data);
   return (
     <div>
       <h1>Home aqui</h1>
