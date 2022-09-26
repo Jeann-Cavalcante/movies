21fa453829b3b01c840a1280e300b05dc38ee81b
Jean <jeanncavalcante@gmail.com>
2022-09-25 21:22:56 -0300

    melhorando titulos

--- src/pages/Series/index.jsx
+++ src/pages/Series/index.jsx
@@ -3,8 +3,8 @@ import Carousel from "../../components/Carousel";
 const Serie = () => {
   return (
     <div className="Content">
-      <Carousel title="Popular" category="tv" list="popular" />
-      <Carousel title="Mais avaliados" category="tv" list="top_rated" />
+      <Carousel title="Populares" category="tv" list="popular" />
+      <Carousel title="Melhores avaliados" category="tv" list="top_rated" />
     </div>
   );
 };
